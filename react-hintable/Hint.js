f12d8dc69249416b00bbfca710170a5aea522375
spencerwiggins <spencer.c.wiggins@gmail.com>
2018-03-02 16:11:18 -0500

    more

--- react-hintable/Hint.js
+++ react-hintable/Hint.js
@@ -1,74 +1,77 @@
-import React from 'react'
-import getHintPosition from './getHintPosition'
-import HintArrow from './HintArrow'
+import React from 'react';
+import getHintPosition from './getHintPosition';
+import HintArrow from './HintArrow';
 
 const style = {
   hintElement: {
     position: 'absolute',
     display: 'flex',
-    alignItems: 'center',
-    zIndex: 100
+    alignItems: 'center'
   }
-}
+};
 
 const directionMap = {
   top: 'column-reverse',
   left: 'row-reverse',
   right: 'row',
   bottom: 'column'
-}
+};
 
-const _defaultPosition = 'bottom'
+const _defaultPosition = 'bottom';
 
 export default class Hint extends React.Component {
   static defaultProps = {
     active: true,
     position: _defaultPosition,
     offset: 10
-  }
+  };
 
   state = {
     hintPosition: {},
     flexDirection: directionMap[this.props.position]
-  }
+  };
 
   setHintPosition = props => {
-    const { target, position, offset } = props || this.props
+    const { target, position, offset } = props || this.props;
 
     const hintPosition = getHintPosition(this.hintElement, target, {
       position,
       offset
-    })
+    });
 
     this.setState({
       hintPosition
-    })
-  }
+    });
+  };
 
   componentWillReceiveProps(nextProps) {
     if (this.props.target !== nextProps.target) {
-      this.setHintPosition(nextProps)
+      this.setHintPosition(nextProps);
     }
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', () => this.setHintPosition(this.props))
-    window.addEventListener('resize', () => this.setHintPosition(this.props))
+    window.addEventListener('scroll', () => this.setHintPosition(this.props));
+    window.addEventListener('resize', () => this.setHintPosition(this.props));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', () => this.setHintPosition(this.props))
-    window.removeEventListener('resize', () => this.setHintPosition(this.props))
+    window.removeEventListener('scroll', () =>
+      this.setHintPosition(this.props)
+    );
+    window.removeEventListener('resize', () =>
+      this.setHintPosition(this.props)
+    );
   }
 
   render() {
-    const { active, children, position } = this.props
+    const { active, children, position } = this.props;
 
     const hintStyle = {
       ...style.hintElement,
       ...this.state.hintPosition,
       flexDirection: this.state.flexDirection
-    }
+    };
 
     return (
       active && (
@@ -77,6 +80,6 @@ export default class Hint extends React.Component {
           <div>{children}</div>
         </div>
       )
-    )
+    );
   }
 }
--- react-hintable/getHintPosition.js
+++ react-hintable/getHintPosition.js
@@ -1,60 +1,54 @@
 // todo: if no reset, body has 8px margin (how to calculate for that)
 
-const topAndBottomLeft = (hint, specs) => {
-  const hintMiddle = half(hint.offsetWidth)
-  const targetMiddle = half(specs.width)
+const _topAndBottomLeft = (hint, specs) => {
+  const hintMiddle = _half(hint.offsetWidth);
+  const targetMiddle = _half(specs.width);
 
-  return specs.left + targetMiddle - hintMiddle
-}
+  return specs.left + targetMiddle - hintMiddle;
+};
 
-const leftAndRightTop = (hint, specs) =>
-  specs.top + half(specs.height - hint.offsetHeight)
+const _leftAndRightTop = (hint, specs) =>
+  specs.top + _half(specs.height - hint.offsetHeight);
 
-const half = x => x / 2
+const _half = x => x / 2;
 
-const top = (hint, specs, offset) => ({
-  top: specs.top - hint.offsetHeight - offset,
-  left: topAndBottomLeft(hint, specs)
-})
-
-const bottom = (hint, specs, offset) => ({
-  top: specs.top + specs.height + half(hint.offsetHeight) - offset,
-  left: topAndBottomLeft(hint, specs)
-})
-
-const left = (hint, specs, offset) => ({
-  top: leftAndRightTop(hint, specs),
-  left: specs.left - hint.offsetWidth - offset
-})
-
-const right = (hint, specs, offset) => ({
-  top: leftAndRightTop(hint, specs),
-  left: specs.left + specs.width + offset
-})
-
-const positionFn = {
-  top,
-  bottom,
-  right,
-  left
-}
-
-const floorSpecValues = specs => {
-  // const out = Object.keys(specs).reduce((acc, val) => {
-  //   console.log(specs[val])
-  //   acc[val] = Math.floor(specs[val])
+const _floorObjVals = obj => {
+  // const out = Object.keys(obj).reduce((acc, val) => {
+  //   console.log(obj[val])
+  //   acc[val] = Math.floor(obj[val])
   //   return acc
   // }, {})
 
   // use above (object.keys) when available
-  let _specs = {}
-  for (let item in specs) {
-    _specs[item] = Math.floor(specs[item])
+  let _obj = {};
+  for (let item in obj) {
+    _obj[item] = Math.floor(obj[item]);
   }
-  return _specs
-}
+  return _obj;
+};
 
 export default (hint, target, { position, offset }) => {
-  const specs = target.getBoundingClientRect()
-  return positionFn[position](hint, floorSpecValues(specs), offset)
-}
+  const rect = target.getBoundingClientRect();
+  const specs = _floorObjVals(rect);
+
+  const map = {
+    top: {
+      top: specs.top - hint.offsetHeight - offset,
+      left: _topAndBottomLeft(hint, specs)
+    },
+    bottom: {
+      top: specs.top + specs.height + _half(hint.offsetHeight) - offset,
+      left: _topAndBottomLeft(hint, specs)
+    },
+    left: {
+      top: _leftAndRightTop(hint, specs),
+      left: specs.left - hint.offsetWidth - offset
+    },
+    right: {
+      top: _leftAndRightTop(hint, specs),
+      left: specs.left + specs.width + offset
+    }
+  };
+
+  return map[position];
+};
