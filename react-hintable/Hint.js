bae52272406e70844da78a9500d983967bb87848
spencerwiggins <spencer.c.wiggins@gmail.com>
2018-03-07 17:28:46 -0500

    hint spec

--- react-hintable/Hint.js
+++ react-hintable/Hint.js
@@ -1,77 +1,81 @@
-import React from 'react';
-import getHintPosition from './getHintPosition';
-import HintArrow from './HintArrow';
+import React from "react"
+import getHintPosition from "./getHintPosition"
+import HintArrow from "./HintArrow"
+import invariant from "invariant"
 
 const style = {
   hintElement: {
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center'
+    position: "absolute",
+    display: "flex",
+    alignItems: "center"
   }
-};
+}
 
 const directionMap = {
-  top: 'column-reverse',
-  left: 'row-reverse',
-  right: 'row',
-  bottom: 'column'
-};
+  top: "column-reverse",
+  left: "row-reverse",
+  right: "row",
+  bottom: "column"
+}
 
-const _defaultPosition = 'bottom';
+const _defaultPosition = "bottom"
 
 export default class Hint extends React.Component {
   static defaultProps = {
     active: true,
     position: _defaultPosition,
     offset: 10
-  };
+  }
+
+  componentWillMount() {
+    invariant(
+      this.props.hasOwnProperty("target"),
+      "You need to pass a target prop to Hint"
+    )
+  }
 
   state = {
     hintPosition: {},
     flexDirection: directionMap[this.props.position]
-  };
+  }
 
   setHintPosition = props => {
-    const { target, position, offset } = props || this.props;
+    const { target, position, offset } = props || this.props
 
     const hintPosition = getHintPosition(this.hintElement, target, {
       position,
       offset
-    });
+    })
 
     this.setState({
       hintPosition
-    });
-  };
+    })
+  }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.target !== nextProps.target) {
-      this.setHintPosition(nextProps);
+      this.setHintPosition(nextProps)
     }
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', () => this.setHintPosition(this.props));
-    window.addEventListener('resize', () => this.setHintPosition(this.props));
+    window.addEventListener("scroll", () => this.setHintPosition(this.props))
+    window.addEventListener("resize", () => this.setHintPosition(this.props))
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', () =>
-      this.setHintPosition(this.props)
-    );
-    window.removeEventListener('resize', () =>
-      this.setHintPosition(this.props)
-    );
+    window.removeEventListener("scroll", () => this.setHintPosition(this.props))
+    window.removeEventListener("resize", () => this.setHintPosition(this.props))
   }
 
   render() {
-    const { active, children, position } = this.props;
+    const { active, children, position } = this.props
 
     const hintStyle = {
       ...style.hintElement,
       ...this.state.hintPosition,
       flexDirection: this.state.flexDirection
-    };
+    }
 
     return (
       active && (
@@ -80,6 +84,6 @@ export default class Hint extends React.Component {
           <div>{children}</div>
         </div>
       )
-    );
+    )
   }
 }
